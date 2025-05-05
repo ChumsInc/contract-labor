9927fa7e60da71a43bad5bd978be0d78d5f192d2
Steve Montgomery <steve@chums.com>
2025-05-05 16:19:56 -0600

    fix: Show work ticket closed alert when needed.

--- src/components/issue-entry/RecalculateIssueStepsButton.tsx
+++ src/components/issue-entry/RecalculateIssueStepsButton.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import {useAppDispatch, useAppSelector} from "@/app/configureStore";
-import {selectWorkTicketSteps} from "@/ducks/work-ticket/currentWorkTicketSlice";
+import {selectWorkTicketHeader, selectWorkTicketSteps} from "@/ducks/work-ticket/currentWorkTicketSlice";
 import {recalculateIssueDetail, selectCurrentIssueHeader} from "@/ducks/issue-entry/issueEntrySlice";
 import Button from "react-bootstrap/Button";
 import {isCLIssue} from "@/utils/issue";
+import Alert from "react-bootstrap/Alert";
 
 export default function RecalculateIssueStepsButton() {
     const dispatch = useAppDispatch();
     const issue = useAppSelector(selectCurrentIssueHeader);
+    const wt = useAppSelector(selectWorkTicketHeader);
     const steps = useAppSelector(selectWorkTicketSteps);
 
     const clickHandler = () => {
         dispatch(recalculateIssueDetail(steps));
     }
 
-    if (!isCLIssue(issue) || !steps.length) {
+    if (!isCLIssue(issue)) {
         return null;
     }
 
+    if (wt?.WorkTicketStatus === 'C') {
+        return (
+           <Alert variant="warning">Work Ticket #{wt.WorkTicketNo.replace(/^0+/, '')} is closed.</Alert>
+        )
+    }
+
     return (
-        <Button type="button" size="sm" variant="outline-warning"
-                onClick={clickHandler}
-                disabled={!!issue.DateReceived}>
-            Recalculate Issue Steps
-        </Button>
+        <>
+            <Button type="button" size="sm" variant="outline-warning"
+                    onClick={clickHandler}
+                    disabled={!!issue.DateReceived || !steps.length}>
+                Recalculate Issue Steps ({steps.length})
+            </Button>
+        </>
     )
 }
